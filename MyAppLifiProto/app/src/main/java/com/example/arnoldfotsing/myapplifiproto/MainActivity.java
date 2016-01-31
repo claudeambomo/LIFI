5e9878a480508b6ed9a01ac7da570b73a54b47dc
aetsmtl <arnold.fotsing.nde@esigetel.net>
2016-01-31 14:17:32 +0100

    Merge branch 'master' of https://github.com/claudeambomo/LIFI

--- MyAppLifiProto/app/src/main/java/com/example/arnoldfotsing/myapplifiproto/MainActivity.java
+++ MyAppLifiProto/app/src/main/java/com/example/arnoldfotsing/myapplifiproto/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.arnoldfotsing.myapplifiproto;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.support.*;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends ActionBarActivity {
         //Commentaire claude
@@ -17,6 +18,52 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configurePosition();
+    }
+
+    public void configurePosition () {
+
+        ImageButton imageButtonPositon = (ImageButton) findViewById(R.id.imageButton2);
+
+        imageButtonPositon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int i=20, j=20, k=20, l=20;
+                ImageButton imageButtonPositon = (ImageButton) findViewById(R.id.imageButton2);
+                for (int h=20; h<1000  ;h+=5){
+
+                    imageButtonPositon.setPadding(i+h,j+h,k+h,l+h);
+                    imageButtonPositon.setBackgroundColor(0x78FFAB11);
+
+                    for (int p=0; p<1000000 ;p++){
+                        //for (int q=0; q<1000000 ;q++){
+                        //}
+                    }
+                }
+            }
+        });
+
+        /*
+        imageButtonPositon.setBackgroundColor(10);
+        System.out.print("color 10");
+        imageButtonPositon.setBackgroundColor(100);
+        System.out.print("color 100");
+        imageButtonPositon.setBackgroundColor(255);
+        System.out.print("color 255");
+
+        int i=20, j=20, k=20, l=20;
+        for (int h=20; h<2000  ;h+=5){
+            imageButtonPositon.setPadding(i+h,j+h,k+h,l+h);
+            for (int p=0; p<1000000 ;p++){
+                //for (int q=0; q<1000000 ;q++){
+                //}
+            }
+        }*/
+
+
+
     }
 
     @Override
@@ -40,4 +87,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
