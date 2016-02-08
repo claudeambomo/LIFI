126f1d4ffeecf7762d97d18b8745b02ba75293ad
claudeambomo <claude.ambomo@esigetel.net>
2016-02-08 19:12:14 +0100

    Commit Claude 08/02/2016 [Avec la page d'accueil]

---
+++ Labyrinthe/app/src/main/java/sdz/chapitreCinq/accueil.java
@@ -0,0 +1,15 @@
+package sdz.chapitreCinq;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by CLAUDE on 08/02/2016.
+ */
+public class accueil extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.accueil_layout);
+    }
+}
