cf617008acf3e2db8061da2dfd77e9fb5bea8538
claudeambomo <claude.ambomo@esigetel.net>
2016-02-08 18:24:50 +0100

    Commit Claude 08/02/2016

--- Labyrinthe/app/src/main/java/sdz/chapitreCinq/Bloc.java
+++ Labyrinthe/app/src/main/java/sdz/chapitreCinq/Bloc.java
@@ -21,6 +21,7 @@ public class Bloc {
 	public Bloc(Type pType, int pX, int pY) {
 		this.mType = pType;
 		this.mRectangle = new RectF(pX * SIZE, pY * SIZE, (pX + 3/2) * SIZE, (pY + 3/2) * SIZE);
+		//Rajouté par Claude (Pour test)
 		/*if (this.mType == Type.ARRIVEE){
 			this.mRectangle = new RectF((pX * SIZE), (pY * SIZE)*3, ((pX + 3/2) * SIZE)*3, ((pY + 3/2) * SIZE));
 		}
