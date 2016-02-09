16cd48fd4ebd02dbecc060869fe67bfa428f7cd8
claudeambomo <claude.ambomo@esigetel.net>
2016-02-09 19:40:14 +0100

    Commit Claude 09/02/2016 [Background sur la deuxieme page]

--- Labyrinthe/app/build/generated/source/r/debug/sdz/chapitreCinq/R.java
+++ Labyrinthe/app/build/generated/source/r/debug/sdz/chapitreCinq/R.java
@@ -1298,7 +1298,8 @@ containing a value of this type.
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020031;
         public static final int abc_textfield_search_material=0x7f020032;
         public static final int ic_launcher=0x7f020033;
-        public static final int lifi_launcher=0x7f020034;
+        public static final int lifi_background=0x7f020034;
+        public static final int lifi_launcher=0x7f020035;
     }
     public static final class id {
         public static final int action_bar=0x7f0b0031;
