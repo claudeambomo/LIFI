ed11148a05f2729ff276d6e2634290a2add90ace
claudeambomo <claude.ambomo@esigetel.net>
2016-02-10 17:06:43 +0100

    Commit Claude 10/02/2016 [1]

--- Labyrinthe/app/src/main/java/sdz/chapitreCinq/accueil.java
+++ Labyrinthe/app/src/main/java/sdz/chapitreCinq/accueil.java
@@ -3,13 +3,16 @@ package sdz.chapitreCinq;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 /**
  * Created by CLAUDE on 08/02/2016.
  */
-public class accueil extends Activity {
+public class accueil extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +26,6 @@ public class accueil extends Activity {
                 startActivity(intent);
             }
         });
+
     }
 }
